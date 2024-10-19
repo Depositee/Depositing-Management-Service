04e0742f8f2407899f743388ca5317a093162aaf
Naphat Chalermwai <naphatchalermwai@gmail.com>
2024-10-19 17:26:13 +0700

    [REMOVE] remove unsued file

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
 import { App } from '@/app';
-import { AuthRoute } from '@routes/auth.route';
-import { UserRoute } from '@routes/users.route';
 import { OrderRoute } from './routes/orders.route';
 import { ValidateEnv } from '@utils/validateEnv';
 
 ValidateEnv();
 
-const app = new App([new AuthRoute(), new OrderRoute()]);
+const app = new App([new OrderRoute()]);
 
 app.listen();
