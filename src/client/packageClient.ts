70317f4203d4e57c9681fd6b9a344544144b7156
Naphat Chalermwai <naphatchalermwai@gmail.com>
2024-10-20 15:18:47 +0700

    add log to packageClient and change proto directory

--- src/client/packageClient.ts
+++ src/client/packageClient.ts
@@ -1,20 +1,31 @@
+/* eslint-disable prettier/prettier */
 import * as grpc from '@grpc/grpc-js';
 import * as protoLoader from '@grpc/proto-loader';
 import path from 'path';
 import { PACKAGE_SERVICE_URL } from '@/config';
+import { logger } from '@utils/logger'; // Assuming you are using a logger utility
 
-const PROTO_PATH = path.join(__dirname, './proto/package.proto'); 
-const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
-  keepCase: true,
-  longs: String,
-  defaults: true,
-  oneofs: true,
-});
+const PROTO_PATH = path.join(__dirname, '../../proto/package.proto');
 
-const packageDefinitionObject = grpc.loadPackageDefinition(packageDefinition) as any; 
-const packageClient = new packageDefinitionObject.package.PackageService(
-    PACKAGE_SERVICE_URL, 
-  grpc.credentials.createInsecure(), 
-);
+let packageClient: any;
+
+try {
+  const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    arrays: true,
+  });
+
+  const packageProto = grpc.loadPackageDefinition(packageDefinition) as any;
+
+  packageClient = new packageProto.package.PackageService(
+    PACKAGE_SERVICE_URL,
+    grpc.credentials.createInsecure(),
+  );
+
+} catch (error) {
+  logger.error('Error initializing gRPC client:', error);
+}
 
 export default packageClient;
